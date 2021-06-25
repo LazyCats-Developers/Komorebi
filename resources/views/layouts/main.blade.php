521c1db7b4430b61bf27310ef65830395f244b9d
Thorristerki <thorristerki@gmail.com>
2021-06-25 05:02:19 -0400

    vista de home con sus cajas listas

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -80,7 +80,8 @@
                 </a>
             </nav>
         </header>
-        <main class="w-full overflow-x-hidden border-t flex flex-col">
+
+        <main class="w-full overflow-x-hidden">
             @yield('content')
         </main>
     </div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,10 +1,45 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="w-full px-5 py-5 flex flex-col items-center space-y-5 md:bg-white md:rounded-xl">
-    <p>
-      Estamos trabajando para usted
-    </p>
-      <img class="w-full xl:w-1/2 rounded-xl" src="../img/working.gif" alt="">
+<div class="grid grid-col space-y-5 p-5">
+
+  <div class="grid grid-col md:grid-cols-2 space-y-5 md:space-y-0 md:space-x-5">
+
+    <div class="grid grid-col md:grid-cols-2 bg-white rounded-xl p-5 md:space-x-5">
+      <div>
+        <img class= "w-full rounded-xl" src="../img/working.gif" alt="">
+      </div>
+      <div class="space-y-5">
+      <p>
+        Bienvenido, Gatito!
+      </p> 
+      <p>
+        aqui estara toda la info personal
+      </p>
+      
+      </div>
+      
+    </div>
+
+    <div class="grid grid-col space-y-5">
+
+      <div class="bg-white rounded-xl p-5">
+        Notificación
+      </div>
+      <div class="bg-white rounded-xl p-5">
+        Notificación
+      </div>
+      <div class="bg-white rounded-xl p-5">
+        Notificación
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="bg-white rounded-xl p-5">
+    CRUD EMPRESAS
+  </div>
+
 </div>
 @endsection
