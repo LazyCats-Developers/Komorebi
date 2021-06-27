681b9445a0ad3a08518538a727ad48ad46c0da24
Thorristerki <thorristerki@gmail.com>
2021-06-26 21:52:37 -0400

    add pages sales, shopping, inventory, cashflow, modules.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,29 +15,29 @@
         <aside>
             <!-- Header Dashboard -->
             <div class="p-6">
-                <a href="" class="text-gray-100 text-3xl font-semibold hover:text-white">
+                <a href="{{ route('login') }}" class="text-gray-100 text-3xl font-semibold hover:text-white">
                 <i class="fas fa-user-cog mr-3"></i>{{ auth()->user()->nombre }}
                 </a>
             </div>
             <!-- Body Dashboard -->
             <nav class="text-white text-base font-semibold pt-3">
-                <a href="" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
+                <a href="{{ url('sales') }}" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
                     <i class="fas fa-cash-register mr-3"></i>
                     Ventas
                 </a>
-                <a href="" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                <a href="{{ url('shopping') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                     <i class="fas fa-table mr-3"></i>
                     Compras
                 </a>
-                <a href="" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                <a href="{{ url('inventory') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                     <i class="fas fa-align-left mr-3"></i>
                     Inventario
                 </a>
-                <a href="" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                <a href="{{ url('cashflow') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                     <i class="fas fa-tablet-alt mr-3"></i>
                     Finanzas
                 </a>
-                <a href="" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                <a href="{{ url('modules') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                     <i class="fas fa-calendar mr-3"></i>
                     Modulos
                 </a>
