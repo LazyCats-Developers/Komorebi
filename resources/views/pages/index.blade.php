1ae5b6f1b5ae504690ef65a9e3e5c7eb1e09e3da
Thorristerki <thorristerki@gmail.com>
2021-06-27 01:49:34 -0400

    add mensaje de crear empresa de forma dinamica

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="flex flex-col space-y-5 p-5">
+<div class="flex flex-col md:space-y-5 md:p-5">
 
   <div class="flex flex-col md:flex-row md:space-x-5">
-
-    <div class="flex w-auto h-auto bg-white rounded-md p-3 space-x-3">
+    <div class="flex w-auto h-auto bg-white md:rounded-md p-3 space-x-3">
       <div>
         <img class= "w-24 rounded-md" src="../img/working.gif" alt="">
       </div>
@@ -17,7 +16,7 @@
       </div>
     </div>
 
-    <div class="flex w-full bg-white rounded-md p-3">
+    <div class="flex w-full bg-white md:rounded-md p-3">
       <div class="flex w-full justify-center items-center">
         <i class="fas fa-cash-register"></i><br>
         <p class="invisible md:visible"> Notificación!</p>
@@ -30,18 +29,6 @@
         <i class="fas fa-cash-register"></i><br>
         <p class="invisible md:visible"> Notificación!</p>
       </div>
-    </div>
-
-  </div>
-
-  <div class="bg-white grid-col space-y-5 p-5 rounded-md">
-    <div class="flex justify-center ">
-      <p>No tienes ninguna empresa registrada aun.</p>
-    </div>
-    <div class="flex justify-center">
-      <a href="{{route('empresas.create')}}" class="flex justify-center w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600 md:max-w-xl">
-        Crear empresa
-      </a>
     </div>
   </div>
 </div>
