a41ae6c7ab22a11d0ae6fb2bff188c1bff50fe40
Thorristerki <thorristerki@gmail.com>
2021-06-29 01:13:57 -0400

    delete ulr error en boton edit en inventory.blade.php

--- resources/views/pages/inventory.blade.php
+++ resources/views/pages/inventory.blade.php
@@ -2,15 +2,40 @@
 
 @section('content')
 <div class="flex flex-col space-y-5 p-5">
-        <a href="{{route('productos.create')}}" class="flex justify-center w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600 md:max-w-xl">
-                Crear items
-        </a>
-        <p>crear el form en views/pages/newitem.blade.php</p>
-        <a href="{{route('productos.edit')}}" class="flex justify-center w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600 md:max-w-xl">
-                Editar items
-        </a>
-        <p>crear el form en views/pages/edititem.blade.php</p>
-        
+        <div>
+                <div class="bg-white grid-col p-3 border-b md:rounded-t-md">
+                        <p>INVENTARIO</p>
+                </div>
+                <div class="bg-white grid grid-col space-y-3 p-3 pb-5 border-b md:space-y-0 md:grid-cols-2">
+                        <a href="{{route('productos.create')}}" class="flex justify-center w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600 md:max-w-xl">
+                                Crear items
+                        </a>
+                        <a href="" class="flex justify-center w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600 md:max-w-xl">
+                                Editar items
+                        </a>
+                </div>
+                <div class="bg-white grid-col space-y-3 p-3 pb-5 border-b">
+                        <p>Datos de la empresa</p>
+                                <div class="grid grid-col space-y-3 md:grid-cols-2 md:space-y-0 md:space-x-3">
+                                        <input type="text" id="nombre" name="nombre" placeholder="Nombre de la empresa" class="border border-gray w-auto rounded-md py-3 px-4" required>
+                                        <input type="text" id="rut" name="rut" placeholder="RUT de la empresa" class="border border-gray w-auto rounded-md py-3 px-4">
+                                </div>
+                        <div class="grid grid-col space-y-3">
+                                <input type="text" id="descripcion" name="descripcion" placeholder="Descripcion de la empresa" class="border border-gray w-full rounded-md py-3 px-4">            
+                        </div>
+                </div>
+                <div class="bg-white grid-col space-y-3 p-3 pb-5 md:rounded-b-md">
+                        <p>Contacto de la empresa</p>
+                        <div class="grid grid-col space-y-3 md:grid-cols-2 md:space-y-0 md:space-x-3">
+                        <input type="text" id="direccion" name="direccion" placeholder="Direccion de la empresa" class="border border-gray w-auto rounded-md py-3 px-4">
+                        <input type="text" id="telefono" name="telefono" placeholder="Telefono de la empresa" class="border border-gray w-auto rounded-md py-3 px-4">
+                        </div>
+                        <div class="grid grid-col space-y-3 md:grid-cols-2 md:space-y-0 md:space-x-3">
+                        <input type="text" id="email" name="email" placeholder="Correo electronico de la empresa" class="border border-gray w-auto rounded-md py-3 px-4">
+                        <input type="text" id="empresa_rrss" name="empresa_rrss" placeholder="Redes sociales de la empresa" class="border border-gray w-auto rounded-md py-3 px-4">
+                        </div>
+                </div>
+        </div>
         <form method="POST" action="">
                 @csrf
                 <div class="form-group">
