862faa5b8a17d1ad99b0aef1e2dff27e2206b046
Thorristerki <thorristerki@gmail.com>
2021-06-29 23:49:11 -0400

    fix views

--- resources/views/pages/modules.blade.php
+++ resources/views/pages/modules.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="flex flex-col space-y-5 p-5">
-    <p>gestion de módulos</p>
-</div>
+    @if(auth()->user()->empresas()->exists())
+        <div class="flex flex-col md:p-3">
+            <div class="bg-white grid-col p-3 border-b md:rounded-t-md">
+                <p>MODULOS</p>
+            </div>
+            <div class="bg-white flex flex-row-reverse p-3 border-b">
+                <a href="" class="flex justify-center w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600 md:max-w-md">
+                    Agregar
+                </a>
+            </div>
+            <div class="bg-white grid grid-col space-y-3 p-3 border-b">
+                <p>Tus modulos</p>
+            </div>
+            <div class="bg-white grid-col p-3 md:rounded-b-md">
+            </div>
+        </div>
+    @endif
 @endsection
--- resources/views/pages/newitem.blade.php
+++ resources/views/pages/newitem.blade.php
@@ -1,27 +1,35 @@
 @extends('layouts.main')
 
 @section('content')
-
-<form method="POST" action="{{route('productos.store')}}">
-    @csrf
-    <div class="form-group">
-        <input  name="nombre" type="text" placeholder="Nombre">
-        <input  name="codigo" type="text" placeholder="Codigo">
-        <input  name="marca" type="text" placeholder="Marca">
-        <input  name="cantidad" type="text" placeholder="Cantidad">
-        <input  name="unidad" type="text" placeholder="Unidad">
-        <input  name="precio_unitario" type="text" placeholder="Precio">
-        <select name="tipo_producto_id">
-            
-            <option value="0" selected>Tipo</option>
-            @foreach ($tipoproductos as $tp)
-            <option value="{{$tp->id}}" >{{$tp->nombre}}</option>
-            @endforeach
-        </select>
-        <input  name="descripcion" type="text" placeholder="Descripcion">
-        <a class="btn btn-primary" href="{{route('tipoproductos.create')}}">Añadir opcion</a>
+    <div class="flex flex-col md:p-3">
+        <div class="bg-gray-400 grid-col p-3 md:rounded-t-md">
+            <p class="text-white">CREAR ITEM</p>
+        </div>
+        <form method="POST" action="{{route('productos.store')}}">
+            @csrf
+            <div class="bg-white flex flex-col space-y-3 p-3 border-b border-gray-300">
+                <input  name="nombre" type="text" placeholder="Nombre" class="border border-gray-300 w-auto rounded-md py-3 px-4">
+                <input  name="codigo" type="text" placeholder="Codigo" class="border border-gray-300 w-auto rounded-md py-3 px-4">
+                <input  name="marca" type="text" placeholder="Marca" class="border border-gray-300 w-auto rounded-md py-3 px-4">
+                <input  name="cantidad" type="text" placeholder="Cantidad" class="border border-gray-300 w-auto rounded-md py-3 px-4">
+                <input  name="unidad" type="text" placeholder="Unidad" class="border border-gray-300 w-auto rounded-md py-3 px-4">
+                <input  name="descripcion" type="text" placeholder="Descripcion" class="border border-gray w-auto rounded-md py-3 px-4">
+                <input  name="precio_unitario" type="text" placeholder="Precio" class="border border-gray-300 w-auto rounded-md py-3 px-4">
+                <select name="tipo_producto_id" class="border border-gray w-auto rounded-md py-3 px-4">
+                    <option value="0" selected>Tipo</option>
+                    @foreach ($tipoproductos as $tp)
+                    <option value="{{$tp->id}}" >{{$tp->nombre}}</option>
+                    @endforeach
+                </select>
+                <a href="{{route('tipoproductos.create')}}" class="w-auto h-auto transition duration-300 ease-in-out transform hover:-translate-y-0 hover:scale-100 bg-gray-400 hover:bg-green--600 text-white font-semibold py-3 px-6 rounded-md md:w-60">
+                    Añadir opcion
+                </a>
+            </div>
+            <div class="bg-white flex flex-row justify-around p-3 md:rounded-b-md">
+                <button class="w-auto h-auto transition duration-300 ease-in-out transform hover:-translate-y-0 hover:scale-100 bg-blue-400 hover:bg-green-600 text-white font-semibold py-3 px-6 rounded-md md:w-60">
+                    Guardar
+                </button>
+            </div>
+        </form>
     </div>
-    <button class="btn btn-success">Guardar</button>
-</form>
-
 @endsection
