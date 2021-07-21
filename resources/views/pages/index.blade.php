90051769f95c083d95ac13416e5da915434bc1a5
Thorristerki <thorristerki@gmail.com>
2021-07-20 20:44:17 -0400

    div profile

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="flex flex-col md:space-y-5 md:p-5">
+    <div class="flex flex-col md:space-y-5 md:p-3">
         <div class="flex flex-col md:flex-row md:space-x-5">
             <div class="flex w-auto h-auto bg-white md:rounded-md p-3 space-x-3">
             <div>
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -2,15 +2,31 @@
 
 @section('content')
 
-    <div>
-        <img src="/uploads/avatars/{{$user->avatar}}" style="width:150px; height:150px; float: left; border-radius:50%; margin-right:25px;">
-        <h2> Perfil de {{$user->nombre}} </h2>
-        <form enctype="multipart/form-data" action="/profile" method="POST">
-            @csrf
-            <label>Subir imagen de perfil</label>
-            <input type="file" name="avatar">
-            <input type="submit" class="pull-right btn btn-sm btn-primary">
-        </form>
+    <div class="flex flex-col md:p-3">
+            <div class="bg-white p-3 border-b md:rounded-t-md">
+                <p>INFORMACION DE LA CUENTA</p>
+            </div>
+            <div class="bg-white flex flex-cols-2 space-y-3 p-3 pb-5 border-b">
+                <div class="w-96">
+                    <img class="w-60 rounded-full" src="/uploads/avatars/{{$user->avatar}}">
+                    <form enctype="multipart/form-data" action="/profile" method="POST">
+                        @csrf
+                        <div class="grid grid-cols-2">
+                        <input type="file" name="avatar">
+                        <input type="submit" class="">
+                        </div>
+
+                    </form>
+                </div>
+                <div>
+                    <h2> Perfil de {{$user->nombre}} </h2>
+                </div>
+            </div>
+            <div class="bg-white grid-col p-3 md:rounded-b-md">
+                <div class="flex flex-row justify-around space-x-3">
+
+                </div>
+            </div>
     </div>
 
 @endsection
