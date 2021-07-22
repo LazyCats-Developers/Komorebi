67576577eca715ad312a39720d5b2736a7e98588
Thorristerki <thorristerki@gmail.com>
2021-07-21 23:19:59 -0400

    Form create item update

--- resources/views/pages/newitem.blade.php
+++ resources/views/pages/newitem.blade.php
@@ -8,32 +8,51 @@
         <form method="POST" action="{{route('productos.store')}}">
             @csrf
             <div class="bg-white flex flex-col space-y-3 p-3 border-b border-gray-300">
-                <div class="grid grid-col md:grid-cols-2 space-y-3 md:space-y-0 md:space-x-3">
-                    <input  name="nombre" type="text" placeholder="Nombre" class="border border-gray-300 w-auto rounded-md py-3 px-4">
-                    <input  name="marca" type="text" placeholder="Marca" class="border border-gray-300 w-auto rounded-md py-3 px-4">
+                <div>
+                    Datos del item
                 </div>
                 <div class="grid grid-col md:grid-cols-2 space-y-3 md:space-y-0 md:space-x-3">
-                    <input  name="unidad" type="text" placeholder="Unidad" class="border border-gray-300 w-auto rounded-md py-3 px-4">
-
-                    <input  name="cantidad" type="text" placeholder="Cantidad" class="border border-gray-300 w-auto rounded-md py-3 px-4">
+                    <input  name="nombre" type="text" placeholder="Nombre" class="border border-gray-300 w-auto rounded-md py-3 px-4" required>
+                    <input  name="marca" type="text" placeholder="Marca" class="border border-gray-300 w-auto rounded-md py-3 px-4" required>
                 </div>
                 <div class="grid grid-col md:grid-cols-2 space-y-3 md:space-y-0 md:space-x-3">
-                    <input  name="codigo" type="text" placeholder="Codigo" class="border border-gray-300 w-auto rounded-md py-3 px-4">
+                    <input  name="codigo" type="text" placeholder="Codigo" class="border border-gray-300 w-auto rounded-md py-3 px-4" required>
                     <input  name="descripcion" type="text" placeholder="Descripcion" class="border border-gray w-auto rounded-md py-3 px-4">
                 </div>
+            </div>
+            <div class="bg-white flex flex-col space-y-3 p-3 border-b border-gray-300">
                 <div class="grid grid-col md:grid-cols-2 space-y-3 md:space-y-0 md:space-x-3">
-                    <input  name="precio_unitario" type="text" placeholder="Precio" class="border border-gray-300 w-auto rounded-md py-3 px-4">
+                    Dosificación del item
+                </div>
+                <div class="grid grid-col md:grid-cols-2 space-y-3 md:space-y-0 md:space-x-3">
+                    <input  name="cantidad" type="text" placeholder="Cantidad" class="border border-gray-300 w-auto rounded-md py-3 px-4" required>
+                    <select  name="unidad" type="text" placeholder="Unidad de medida" class="border border-gray-300 w-auto rounded-md py-3 px-4" required>
+                        <option value="1">Unidad</option>
+                        <option value="2">Kilogramo</option>
+                        <option value="3">Gramos</option>
+                        <option value="4">Litros</option>
+                        <option value="5">Mililitros</option>
+                        <option value="6">Onsas</option>
+                    </select>
+                </div>
+            </div>
+            <div class="bg-white flex flex-col space-y-3 p-3 border-b border-gray-300">
+                <div>
+                    Tipo del item
+                </div>
+                <div class="grid grid-col md:grid-cols-2 space-y-3 md:space-y-0 md:space-x-3">
                     <select name="tipo_producto_id" class="border border-gray w-auto rounded-md py-3 px-4">
-                        <option value="0" selected>Tipo</option>
                         @foreach ($tipoproductos as $tp)
                         <option value="{{$tp->id}}" >{{$tp->nombre}}</option>
                         @endforeach
                     </select>
                 </div>
-                <div class="grid grid-col justify-items-end space-y-3 md:space-y-0 md:space-x-3">
-                    <a href="{{route('tipoproductos.create')}}" class="flex justify-center w-full bg-white-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-gray-100 md:max-w-xs">
-                        Añadir tipo
-                    </a>
+                <div class="grid grid-col md:grid-cols-2 space-y-3 md:space-y-0 md:space-x-3">
+                    <input  name="precio_unitario" type="text" placeholder="Costo/Valor Venta" class="border border-gray-300 w-auto rounded-md py-3 px-4" required>
+                    <input  name="stock_inicial" type="text" placeholder="Stock inicial" class="border border-gray-300 w-auto rounded-md py-3 px-4">
+                </div>
+                <div class="space-y-3 md:space-y-0 md:space-x-3">
+                    <label for="iva">Este item contiene IVA incluido</label> <input type="checkbox" name="iva" id="iva">
                 </div>
             </div>
             <div class="bg-white flex flex-row justify-around p-3 md:rounded-b-md">
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -7,24 +7,37 @@
                 <p>INFORMACION DE LA CUENTA</p>
             </div>
             <div class="bg-white flex flex-cols-2 space-y-3 p-3 pb-5 border-b">
-                <div class="w-96">
-                    <img class="w-60 rounded-full" src="/uploads/avatars/{{$user->avatar}}">
-                    <form enctype="multipart/form-data" action="/profile" method="POST">
+                <form enctype="multipart/form-data" action="/profile" method="POST">
+                    <div class="w-96 grid grid-col space-y-3 justify-items-center">
+                        <img class="w-60 rounded-full" src="/uploads/avatars/{{$user->avatar}}">
                         @csrf
-                        <div class="grid grid-cols-2">
-                        <input type="file" name="avatar">
-                        <input type="submit" class="">
+                        <input class="bg-gray-100 w-60" type="file" name="avatar">
+                        <input class="min-w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md sm: hover:bg-green-600" type="submit">
+                    </div>
+                </form>
+                <div class="p-3">
+                    <h2> Perfil de {{$user->nombre}} </h2>
+
+                        <div class="w-full bg-white flex-col flex justify-center items-center sm:w-96 sm:rounded-xl">
+                            <form class="space-y-5" action="" method="POST">
+                                @csrf
+                                <input type="text" id="nombre" name="nombre" placeholder="Nombre" class="border border-gray w-full rounded-md py-3 px-4">
+                                <input type="text" id="apellido" name="apellido" placeholder="Apellido" class="border border-gray w-full rounded-md py-3 px-4">
+                                <input type="text" id="telefono" name="telefono" placeholder="Telefono" class="border border-gray w-full rounded-md py-3 px-4">
+                                <input type="text" id="direccion" name="direccion" placeholder="Direccion" class="border border-gray w-full rounded-md py-3 px-4">
+                                <input type="text" id="email" name="email" placeholder="Correo electrónico" class="border border-gray w-full rounded-md py-3 px-4">
+                                <input type="password" id="password" name="password" placeholder="Contraseña" class="border border-gray w-full rounded-md py-3 px-4">
+                                <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirmar Contraseña" class="border border-gray w-full rounded-md py-3 px-4">
+                                <button type="submit" class="min-w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600">
+                                    Actualizar datos
+                                </button>
+                            </form>
                         </div>
 
-                    </form>
-                </div>
-                <div>
-                    <h2> Perfil de {{$user->nombre}} </h2>
                 </div>
             </div>
             <div class="bg-white grid-col p-3 md:rounded-b-md">
                 <div class="flex flex-row justify-around space-x-3">
-
                 </div>
             </div>
     </div>
