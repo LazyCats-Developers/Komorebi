5b7de543ff4f4f1bf96534ad904049dff74c7f1f
Thorristerki <thorristerki@gmail.com>
2021-07-22 23:58:57 -0400

    new form sellitem

--- resources/views/pages/newsales.blade.php
+++ resources/views/pages/newsales.blade.php
@@ -7,49 +7,65 @@
                 <p>CARRITO DE VENTAS</p>
             </div>
         </div>
-        <div class="bg-white flex flex-row space-x-3 p-3 border-b">
-            <a href="" class="flex justify-center w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600 md:max-w-md">
+        <div class="bg-white flex flex-row space-x-3 p-3 border-b justify-between">
+            <a href="" class="flex justify-center w-full bg-green-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600 md:w-64">
                 Crear venta
             </a>
-            <a href="" class="flex justify-center w-full bg-red-300 text-white font-semibold py-3 px-6 rounded-md hover:bg-red-600 md:max-w-md">
+            <a href="" class="flex justify-center w-full bg-red-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-red-600 md:w-64">
                 Cancelar venta
             </a>
         </div>
-        <div class="bg-white flex flex-col space-y-3 p-3 border-b md:space-y-0 md:space-x-3 md:flex-row">
-            <input type="text" class="border border-gray w-full rounded-md py-3 px-4 md:w-60">
-            <button class="w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md md:w-60 hover:bg-green-600">
-                buscar
+        <div class="bg-white flex flex-col space-y-3 p-3 border-b md:space-y-0 md:space-x-3 md:flex-row md:justify-between">
+            <div class="space-y-3 md:space-y-0 md:space-x-3">
+                <input type="text" placeholder="Ingresar codigo" class="border border-gray w-full rounded-md py-3 px-4 md:w-64">
+                <button class="w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md md:w-40 hover:bg-blue-600">
+                    Buscar
+                </button>
+            </div>
+            <div>
+                <p class="font-bold">Total Venta</p>
+                <input type="text" value="700" placeholder="Total venta" class="text-right border border-gray w-full rounded-md py-3 px-4 md:w-64" disabled>
+            </div>
+        </div>
+        <div class="bg-white flex flex-col space-y-3 p-3 border-b md:space-y-0 md:space-x-3 md:flex-row md:justify-between">
+            <div class="space-y-3 md:space-y-0 md:space-x-3">
+                <input type="text" placeholder="Nombre item" class="border border-gray w-full rounded-md py-3 px-4 md:w-64" disabled>
+                <input type="text" placeholder="Valor del item" class="border border-gray w-full rounded-md py-3 px-4 md:w-40" disabled>
+                <input type="text" placeholder="Ingresar cantidad" class="border border-gray w-full rounded-md py-3 px-4 md:w-40 ">
+            </div>
+            <button class="w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md md:w-64 hover:bg-blue-600">
+                Agregar item
             </button>
         </div>
-        <div class="bg-white space-y-3 p-3">
+        <div class="bg-white space-y-3 py-3 border-b">
             <table class="w-full">
                 <thead class="bg-gray-300">
-                    <tr>
+                    <tr class="grid grid-cols-7 justify-items-center">
                         <th>Codigo</th>
                         <th>Nombre</th>
                         <th>valor</th>
+                        <th>Cantidad</th>
+                        <th>SubTotal</th>
                         <th>Editar</th>
                         <th>Eliminar</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>002525</td>
+                    <tr class="grid grid-cols-7 justify-items-center">
+                        <td>9999999</td>
                         <td>Chokita</td>
                         <td>350</td>
+                        <td>2</td>
+                        <td>700</td>
                         <td>
-                            <a class="btn btn-warning" href="">
-                                    <i class="fa fa-edit"></i>
-                            </a>
+                            <button class="btn btn-warning" href="">
+                                <i class="fa fa-edit"></i>
+                            </button>
                         </td>
                         <td>
-                            <form action="" method="post">
-                                @method("delete")
-                                @csrf
-                                <button type="submit" class="btn btn-danger">
-                                    <i class="fa fa-trash"></i>
-                                </button>
-                            </form>
+                            <button type="submit" class="btn btn-danger">
+                                <i class="fa fa-trash"></i>
+                            </button>
                         </td>
                     </tr>
                 </tbody>
--- resources/views/pages/sales.blade.php
+++ resources/views/pages/sales.blade.php
@@ -3,42 +3,39 @@
 @section('content')
     @if(auth()->user()->empresas()->exists())
         <div class="flex flex-col md:p-3">
-            <div>
-                <div class="bg-white grid-col p-3 border-b md:rounded-t-md">
-                    <p>VENTAS</p>
-                </div>
-                <div class="bg-white flex flex-row-reverse p-3 border-b">
-                    <a href="{{ url('newsales') }}" class="flex justify-center w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600 md:max-w-md">
-                        Crear venta
-                    </a>
-                </div>
-                <div class="bg-white grid grid-col space-y-3 p-3 border-b">
-                    <p>Consulta de ventas</p>
-                    <table class="">
-                        <thead class="bg-gray-300">
-                            <tr>
-                                <th>ID</th>
-                                <th>fecha</th>
-                                <th>hora</th>
-                                <th>valor</th>
-                                <th>vendedor</th>
-                                <th>tipo</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="bg-white grid-col p-3 md:rounded-b-md">
-                </div>
+            <div class="bg-white grid-col p-3 border-b md:rounded-t-md">
+                <p>VENTAS</p>
+            </div>
+            <div class="bg-white flex flex-row-reverse p-3 border-b">
+                <a href="{{ url('newsales') }}" class="flex justify-center w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600 md:max-w-md">
+                    Crear venta
+                </a>
+            </div>
+            <div class="bg-white space-y-3 py-3 border-b">
+                <table class="w-full">
+                    <thead class="bg-gray-300">
+                        <tr class="grid grid-cols-7 justify-items-center">
+                            <th>ID</th>
+                            <th>fecha</th>
+                            <th>hora</th>
+                            <th>valor</th>
+                            <th>vendedor</th>
+                            <th>tipo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="grid grid-cols-7 justify-items-center">
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="bg-white p-3 md:rounded-b-md">
             </div>
         </div>
     @endif
