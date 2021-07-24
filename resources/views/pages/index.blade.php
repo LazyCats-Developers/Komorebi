b3d3942e4b599b0767358823e0674268f236ae87
Thorristerki <thorristerki@gmail.com>
2021-07-23 21:48:21 -0400

    new viws profile

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,82 +1,79 @@
 @extends('layouts.main')
 
 @section('content')
-        @if(auth()->user()->empresas()->exists())
-            <div class="flex flex-col md:p-5 items-center">
-                <div class="w-full max-w-7xl">
-                    <div class="flex flex-col space-y-3 md:flex-row md:space-y-0 md:justify-between bg-gray-50 shadow-lg grid-col p-2 border-b md:rounded-t-3xl">
-                        <p class="font-bold text-xl"><i class="fas fa-chart-line p-3 bg-white rounded-full border"></i> FINANZAS</p>
-                        <a href="" class="flex justify-center w-full bg-gradient-to-r from-green-300 to-green-500 text-white text-xl hover:from-green-600 hover:to-green-600 focus:outline-none  p-2 rounded-full hover:bg-green-600 md:w-72 invisible">
-                            Nueva venta
-                        </a>
-                    </div>
-                    <div class="bg-white shadow-lg flex flex-col space-y-3 pt-3 px-3 md:space-y-0 md:flex-row md:justify-between">
-                        <div class="flex flex-col space-y-3 md:flex-row md:space-y-0">
-                            <div>
-                                <img class= "w-24 h-24 rounded-full" src="../img/working.gif" alt="">
-                            </div>
-                            <div class="md:w-80">
-                                <p>Bienvenido, {{ auth()->user()->nombre }} </p>
-                                <p>
-                                @if(auth()->user()->empresas()->exists())
-                                    {{ auth()->user()->colaboradores()->first()->cargo_usuario }} de {{ auth()->user()->empresas()->first()->nombre }}
-                                @else
-                                    Aún no tienes empresa registrada
-                                @endif
-                                </p>
-                                <a href="{{ route('profile') }}">
-                                    <p class="text-sm text-blue-500">Editar tu perfil</p>
-                                </a>
-                            </div>
+    @if(auth()->user()->empresas()->exists())
+        <div class="flex flex-col md:p-5 items-center">
+            <div class="w-full max-w-7xl">
+                <div class="flex flex-col space-y-3 md:flex-row md:space-y-0 md:justify-between bg-gray-50 shadow-lg grid-col p-2 border-b md:rounded-t-3xl">
+                    <p class="font-bold text-xl"><i class="fas fa-chart-line p-3 bg-white rounded-full border"></i> FINANZAS</p>
+                    <a href="" class="flex justify-center w-full bg-gradient-to-r from-green-300 to-green-500 text-white text-xl hover:from-green-600 hover:to-green-600 focus:outline-none  p-2 rounded-full hover:bg-green-600 md:w-72 invisible">
+                        Nueva venta
+                    </a>
+                </div>
+                <div class="bg-white shadow-lg flex flex-col space-y-3 pt-3 px-3 md:space-y-0 md:flex-row md:justify-between">
+                    <div class="flex flex-col space-y-3 md:flex-row md:space-y-0">
+                        <div>
+                            <img class= "w-24 h-24 rounded-full" src="../img/working.gif" alt="">
                         </div>
-                        <div class="flex flex-row">
-                            <div class="flex w-full bg-white md:rounded-md p-3">
-                                <div class="w-full bg-blue-400 text-white py-3 px-6 rounded-l-full md:w-24 hover:bg-blue-500">
-                                    <i class="fas fa-exclamation-circle"></i>
-                                    <p class="invisible md:visible">Notificación!</p>
-                                </div>
-                                <div class="w-full bg-blue-400 text-white py-3 px-6 md:w-24 hover:bg-blue-500">
-                                    <i class="fas fa-exclamation-circle"></i>
-                                    <p class="invisible md:visible">Notificación!</p>
-                                </div>
-                                <div class="w-full bg-blue-400 text-white py-3 px-6 rounded-r-full  md:w-24 hover:bg-blue-500">
-                                    <i class="fas fa-exclamation-circle"></i><br>
-                                    <p class="invisible md:visible">Notificación!</p>
-                                </div>
-                            </div>
+                        <div class="md:w-80">
+                            <p>Bienvenido, {{ auth()->user()->nombre }} </p>
+                            <p>
+                            @if(auth()->user()->empresas()->exists())
+                                {{ auth()->user()->colaboradores()->first()->cargo_usuario }} de {{ auth()->user()->empresas()->first()->nombre }}
+                            @else
+                                Aún no tienes empresa registrada
+                            @endif
+                            </p>
+                            <a href="{{ route('profile') }}">
+                                <p class="text-sm text-blue-500">Editar tu perfil</p>
+                            </a>
                         </div>
                     </div>
-                    <div class="bg-white shadow-lg space-y-3 p-3 border-b">
-                        <table class="w-full">
-                            <thead>
-                                <tr class="grid grid-cols-6 justify-items-center bg-gray-100 border rounded-t-3xl">
-                                    <th>ID</th>
-                                    <th>Fecha</th>
-                                    <th>Hora</th>
-                                    <th>Valor</th>
-                                    <th>Vendedor</th>
-                                    <th>Tipo</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr class="grid grid-cols-6 justify-items-center border rounded-b-3xl">
-                                    <td>1111111</td>
-                                    <td>01/01/0001</td>
-                                    <td>01:01</td>
-                                    <td>1000</td>
-                                    <td>Yisus</td>
-                                    <td>Factura</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="bg-gray-50 shadow-lg p-2 md:rounded-b-3xl">
-                        <p class="text-center text-gray-400">Komorebi</p>
+                    <div class="flex flex-row">
+                        <div class="flex w-full bg-white md:rounded-md p-3">
+                            <div class="w-full bg-blue-400 text-white py-3 px-6 rounded-l-full md:w-24 hover:bg-blue-500">
+                                <i class="fas fa-exclamation-circle"></i>
+                                <p class="invisible md:visible">Notificación!</p>
+                            </div>
+                            <div class="w-full bg-blue-400 text-white py-3 px-6 md:w-24 hover:bg-blue-500">
+                                <i class="fas fa-exclamation-circle"></i>
+                                <p class="invisible md:visible">Notificación!</p>
+                            </div>
+                            <div class="w-full bg-blue-400 text-white py-3 px-6 rounded-r-full  md:w-24 hover:bg-blue-500">
+                                <i class="fas fa-exclamation-circle"></i><br>
+                                <p class="invisible md:visible">Notificación!</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <div class="bg-white shadow-lg space-y-3 p-3 border-b">
+                    <table class="w-full">
+                        <thead>
+                            <tr class="grid grid-cols-6 justify-items-center bg-gray-100 border rounded-t-3xl">
+                                <th>ID</th>
+                                <th>Fecha</th>
+                                <th>Hora</th>
+                                <th>Valor</th>
+                                <th>Vendedor</th>
+                                <th>Tipo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="grid grid-cols-6 justify-items-center border rounded-b-3xl">
+                                <td>1111111</td>
+                                <td>01/01/0001</td>
+                                <td>01:01</td>
+                                <td>1000</td>
+                                <td>Yisus</td>
+                                <td>Factura</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="bg-gray-50 shadow-lg p-2 md:rounded-b-3xl">
+                    <p class="text-center text-gray-400">Komorebi</p>
+                </div>
             </div>
-
-
-        @endif
-    </div>
+        </div>
+    @endif
 @endsection
--- resources/views/pages/newshop.blade.php
+++ resources/views/pages/newshop.blade.php
@@ -1,82 +1,81 @@
 @extends('layouts.main')
 
 @section('content')
-        <div class="flex flex-col md:p-5 items-center">
-            <div class="w-full max-w-7xl">
-                <div class="flex flex-col space-y-3 md:flex-row md:space-y-0 md:justify-between bg-gray-50 shadow-lg grid-col p-2 border-b md:rounded-t-3xl">
-                    <div  class="flex flex-col space-y-3 md:flex-row md:space-y-0">
-                        <p class="font-bold text-xl"><i class="fas fa-ambulance p-3 bg-white rounded-full border"></i> INGRESO DE INSUMOS</p>
-                    </div>
-                    <div class="flex flex-row gap-3">
-                        <button class="flex justify-center w-full bg-gradient-to-r from-green-300 to-green-500 text-white text-xl hover:from-green-600 hover:to-green-600 focus:outline-none  p-2 rounded-full hover:bg-green-600 md:w-72">
-                            Ingresar compra
-                        </button>
-                        <button class="flex justify-center w-full bg-gradient-to-r from-red-300 to-red-500 text-white text-xl hover:from-green-600 hover:to-green-600 focus:outline-none  p-2 rounded-full hover:bg-green-600 md:w-72">
-                            Cancelar
-                        </button>
-                    </div>
+    <div class="flex flex-col md:p-5 items-center">
+        <div class="w-full max-w-7xl">
+            <div class="flex flex-col space-y-3 md:flex-row md:space-y-0 md:justify-between bg-gray-50 shadow-lg grid-col p-2 border-b md:rounded-t-3xl">
+                <div  class="flex flex-col space-y-3 md:flex-row md:space-y-0">
+                    <p class="font-bold text-xl"><i class="fas fa-ambulance p-3 bg-white rounded-full border"></i> INGRESO DE INSUMOS</p>
                 </div>
-                <div class="bg-white shadow-lg flex flex-col space-y-3 pt-3 px-3 md:space-y-0 md:flex-row md:justify-between">
-                    <div class="flex flex-col space-y-3 md:flex-row md:space-y-0">
-                        <input type="text" placeholder="Codigo item" class="w-full placeholder-gray-400 px-6 py-3 border border-gray-300 md:rounded-l-full md:w-64">
-                        <button class="bg-blue-400 w-full text-white p-3 rounded-3xl md:rounded-r-full md:w-36 hover:bg-blue-500">
-                            Buscar item
-                        </button>
-                    </div>
-                    <div class="flex flex-row gap-2">
-                        <p class="w-24 placeholder-gray-400 px-6 py-3 border border-gray-300 text-center md:rounded-l-full">Total</p>
-                        <input type="text" value="700" placeholder="Total Compra" class="w-full placeholder-gray-400 px-6 py-3 border border-gray-300 text-right md:rounded-r-full md:w-40" disabled>
-                    </div>
+                <div class="flex flex-row gap-3">
+                    <button class="flex justify-center w-full bg-gradient-to-r from-green-300 to-green-500 text-white text-xl hover:from-green-600 hover:to-green-600 focus:outline-none  p-2 rounded-full hover:bg-green-600 md:w-72">
+                        Ingresar compra
+                    </button>
+                    <button class="flex justify-center w-full bg-gradient-to-r from-red-300 to-red-500 text-white text-xl hover:from-green-600 hover:to-green-600 focus:outline-none  p-2 rounded-full hover:bg-green-600 md:w-72">
+                        Cancelar
+                    </button>
                 </div>
-                <div class="bg-white shadow-lg flex flex-col space-y-3 pt-3 px-3 md:space-y-0 md:flex-row md:justify-between">
-                    <div class="flex flex-col space-y-3 md:flex-row md:space-y-0">
-                        <input type="text" placeholder="Nombre item" class="w-full placeholder-gray-400 px-6 py-3 border border-gray-300 md:rounded-l-full md:w-40" disabled>
-                        <input type="text" placeholder="Codigo item" class="w-full placeholder-gray-400 px-6 py-3 border border-gray-300 md:w-40" disabled>
-                        <input type="text" placeholder="Ingresar Cantidad" class="w-full placeholder-gray-400 px-6 py-3 border border-gray-300 md:rounded-r-full md:w-44">
-                    </div>
-                    <div class="flex flex-row">
-                        <button class="w-full bg-blue-400 text-white py-3 px-6 rounded-full  md:w-52 hover:bg-blue-500">
-                            Agregar item
-                        </button>
-                    </div>
+            </div>
+            <div class="bg-white shadow-lg flex flex-col space-y-3 pt-3 px-3 md:space-y-0 md:flex-row md:justify-between">
+                <div class="flex flex-col space-y-3 md:flex-row md:space-y-0">
+                    <input type="text" placeholder="Codigo item" class="w-full placeholder-gray-400 px-6 py-3 border border-gray-300 md:rounded-l-full md:w-64">
+                    <button class="bg-blue-400 w-full text-white p-3 rounded-3xl md:rounded-r-full md:w-36 hover:bg-blue-500">
+                        Buscar item
+                    </button>
                 </div>
-                <div class="bg-white shadow-lg space-y-3 p-3 border-b">
-                    <table class="w-full">
-                        <thead>
-                            <tr class="grid grid-cols-7 justify-items-center bg-gray-100 border rounded-t-3xl">
-                                <th>Codigo</th>
-                                <th>Nombre</th>
-                                <th>valor</th>
-                                <th>Cantidad</th>
-                                <th>SubTotal</th>
-                                <th>Editar</th>
-                                <th>Eliminar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="grid grid-cols-7 justify-items-center border rounded-b-3xl">
-                                <td>9999999</td>
-                                <td>Chokita</td>
-                                <td>350</td>
-                                <td><input type="text" value="2" class="text-center"></td>
-                                <td>700</td>
-                                <td>
-                                    <button class="btn btn-warning" href="">
-                                        <i class="fa fa-edit"></i>
-                                    </button>
-                                </td>
-                                <td>
-                                    <button type="submit" class="btn btn-danger">
-                                        <i class="fa fa-trash"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="flex flex-row gap-2">
+                    <p class="w-24 placeholder-gray-400 px-6 py-3 border border-gray-300 text-center md:rounded-l-full">Total</p>
+                    <input type="text" value="700" placeholder="Total Compra" class="w-full placeholder-gray-400 px-6 py-3 border border-gray-300 text-right md:rounded-r-full md:w-40" disabled>
                 </div>
-                <div class="bg-gray-50 shadow-lg p-2 md:rounded-b-3xl">
-                    <p class="text-center text-gray-400">Komorebi</p>
+            </div>
+            <div class="bg-white shadow-lg flex flex-col space-y-3 pt-3 px-3 md:space-y-0 md:flex-row md:justify-between">
+                <div class="flex flex-col space-y-3 md:flex-row md:space-y-0">
+                    <input type="text" placeholder="Nombre item" class="w-full placeholder-gray-400 px-6 py-3 border border-gray-300 md:rounded-l-full md:w-40" disabled>
+                    <input type="text" placeholder="Codigo item" class="w-full placeholder-gray-400 px-6 py-3 border border-gray-300 md:w-40" disabled>
+                    <input type="text" placeholder="Ingresar Cantidad" class="w-full placeholder-gray-400 px-6 py-3 border border-gray-300 md:rounded-r-full md:w-44">
                 </div>
+                <div class="flex flex-row">
+                    <button class="w-full bg-blue-400 text-white py-3 px-6 rounded-full  md:w-52 hover:bg-blue-500">
+                        Agregar item
+                    </button>
+                </div>
+            </div>
+            <div class="bg-white shadow-lg space-y-3 p-3 border-b">
+                <table class="w-full">
+                    <thead>
+                        <tr class="grid grid-cols-7 justify-items-center bg-gray-100 border rounded-t-3xl">
+                            <th>Codigo</th>
+                            <th>Nombre</th>
+                            <th>valor</th>
+                            <th>Cantidad</th>
+                            <th>SubTotal</th>
+                            <th>Editar</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="grid grid-cols-7 justify-items-center border rounded-b-3xl">
+                            <td>9999999</td>
+                            <td>Chokita</td>
+                            <td>350</td>
+                            <td><input type="text" value="2" class="text-center"></td>
+                            <td>700</td>
+                            <td>
+                                <button class="btn btn-warning" href="">
+                                    <i class="fa fa-edit"></i>
+                                </button>
+                            </td>
+                            <td>
+                                <button type="submit" class="btn btn-danger">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="bg-gray-50 shadow-lg p-2 md:rounded-b-3xl">
+                <p class="text-center text-gray-400">Komorebi</p>
             </div>
         </div>
     </div>
