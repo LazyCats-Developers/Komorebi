a1a8316598fdf305c4406cbe03e7baf12a9542f2
Thorristerki <thorristerki@gmail.com>
2021-07-26 01:04:11 -0400

    responsive views in login and register forms

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,22 +9,22 @@
 </head>
 <body>
 <title>Iniciar sesión en Komorebi</title>
-<div class="bg-white min-h-screen w-full flex-col flex justify-center items-center space-y-5 sm:bg-blue-300">
-    <div class="w-full bg-white flex-col flex justify-center items-center p-5 lg:w-72 2xl:w-96 sm:rounded-xl">
-        <form method="POST" class="space-y-5" action="{{ route('login') }}">
+<div class="bg-white min-h-screen w-full flex-col flex justify-center items-center gap-3 sm:bg-blue-300">
+    <div class="w-full bg-white flex-col flex justify-center items-center p-5 sm:rounded-xl sm:w-72 xl:w-80 2xl:w-96 ">
+        <form method="POST" class="space-y-3" action="{{ route('login') }}">
             @csrf
             <img class="min-w-full mb-5" src="{{ asset('img/logo.png') }}" alt="">
-            <input type="email" id="email" name="email" placeholder="Correo Electronico" class="border border-gray w-full rounded-md py-3 px-4 text-sm 2xl:text-lg" required>
-            <input type="password" id="password" name="password" placeholder="Contraseña" class="border border-gray w-full rounded-md py-3 px-4 text-sm 2xl:text-lg" required>
+            <input type="email" id="email" name="email" placeholder="Correo Electronico" class="border border-gray w-full rounded-md py-3 px-4 text-md 2xl:text-lg" required>
+            <input type="password" id="password" name="password" placeholder="Contraseña" class="border border-gray w-full rounded-md py-3 px-4 text-md 2xl:text-lg" required>
             <input type="checkbox" id="remember" name='remember'><label for="remember" class="text-sm 2xl:text-lg"> Recuérdame</label>
             <button class="min-w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600">
                 Iniciar sesión
             </button>
         </form>
     </div>
-    <div class="w-full bg-white flex justify-center items-center p-5 space-x-2 lg:w-72 2xl:w-96 sm:rounded-xl">
-        <p class="text-gray-700 text-sm 2xl:text-lg">¿No tienes una cuenta?</p>
-        <a class="text-blue-500 text-sm 2xl:text-lg" href="{{ route('signup') }}">Regístrate</a>
+    <div class="w-full bg-white flex justify-center items-center p-3 space-x-2 sm:rounded-xl sm:w-72 xl:w-80 2xl:w-96 ">
+        <p class="text-gray-700 font-medium text-md xl:text-lg 2xl:text-xl">¿No tienes una cuenta?</p>
+        <a class="text-blue-500 font-medium text-md xl:text:lg 2xl:text-xl" href="{{ route('signup') }}">Regístrate</a>
     </div>
 </div>
 </body>
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -9,30 +9,30 @@
 </head>
 <body>
 <title>Registrarte en Komorebi</title>
-<div class="bg-white sm:bg-blue-300 min-h-screen flex-col flex justify-center items-center space-y-5">
-    <div class="w-full bg-white flex-col flex justify-center items-center p-5 sm:w-96 sm:rounded-xl">
+<div class="bg-white sm:bg-blue-300 min-h-screen flex-col flex justify-center items-center gap-3">
+    <div class="w-full bg-white flex-col flex justify-center items-center p-3 sm:w-72 xl:w-80 2xl:w-96 sm:rounded-xl">
 
-        <form class="space-y-5" action="{{ url('/signup') }}" method="POST">
+        <form class="space-y-3" action="{{ url('/signup') }}" method="POST">
             @csrf
             <div class="min-w-full flex-col flex justify-center items-center mb-5">
                 <img class="min-w-full" src="../../img/logo.png" alt="">
-                <p class="text-xl font-medium text-gray-700">Regístrate para crear una cuenta</p>
+                <p class="text-gray-700 font-medium text-md   xl:text-lg 2xl:text-xl">Regístrate para crear una cuenta</p>
             </div>
-            <input type="text" id="nombre" name="nombre" placeholder="Nombre" class="border border-gray w-full rounded-md py-3 px-4">
-            <input type="text" id="apellido" name="apellido" placeholder="Apellido" class="border border-gray w-full rounded-md py-3 px-4">
-            <input type="text" id="telefono" name="telefono" placeholder="Telefono" class="border border-gray w-full rounded-md py-3 px-4">
-            <input type="text" id="direccion" name="direccion" placeholder="Direccion" class="border border-gray w-full rounded-md py-3 px-4">
-            <input type="text" id="email" name="email" placeholder="Correo electrónico" class="border border-gray w-full rounded-md py-3 px-4">
-            <input type="password" id="password" name="password" placeholder="Contraseña" class="border border-gray w-full rounded-md py-3 px-4">
-            <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirmar Contraseña" class="border border-gray w-full rounded-md py-3 px-4">
+            <input type="text" id="nombre" name="nombre" placeholder="Nombre" class="border border-gray w-full rounded-md py-3 px-4 2xl:text-lg">
+            <input type="text" id="apellido" name="apellido" placeholder="Apellido" class="border border-gray w-full rounded-md py-3 px-4 text-md 2xl:text-lg">
+            <input type="text" id="telefono" name="telefono" placeholder="Telefono" class="border border-gray w-full rounded-md py-3 px-4 text-md 2xl:text-lg">
+            <input type="text" id="direccion" name="direccion" placeholder="Direccion" class="border border-gray w-full rounded-md py-3 px-4 text-md 2xl:text-lg">
+            <input type="text" id="email" name="email" placeholder="Correo electrónico" class="border border-gray w-full rounded-md py-3 px-4 text-md 2xl:text-lg">
+            <input type="password" id="password" name="password" placeholder="Contraseña" class="border border-gray w-full rounded-md py-3 px-4 text-md 2xl:text-lg">
+            <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirmar Contraseña" class="border border-gray w-full rounded-md py-3 px-4 text-md 2xl:text-lg">
             <button type="submit" class="min-w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600">
                 Registrar
             </button>
         </form>
     </div>
-    <div class="w-full bg-white flex justify-center items-center p-5 space-x-2 sm:w-96 sm:rounded-xl">
-        <p class="text-lg font-medium text-gray-700">¿Tienes una cuenta?</p>
-        <a class="text-lg font-medium text-blue-500" href="{{url('/')}}">Inicia sesión</a>
+    <div class="w-full bg-white flex justify-center items-center p-3 space-x-2 sm:w-72 xl:w-80 2xl:w-96 sm:rounded-xl">
+        <p class="text-gray-700 font-medium  text-md xl:text-lg 2xl:text-xl">¿Tienes una cuenta?</p>
+        <a class="text-blue-500 font-medium 0 text-md xl:text-lg 2xl:text-xl" href="{{url('/')}}">Inicia sesión</a>
     </div>
 </div>
 </body>
