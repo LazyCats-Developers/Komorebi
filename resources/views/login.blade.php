d021046fce938590f3530bdee6ce4b838fb7ac57
Thorristerki <thorristerki@gmail.com>
2021-07-25 23:35:25 -0400

    sw views login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,21 +10,21 @@
 <body>
 <title>Iniciar sesión en Komorebi</title>
 <div class="bg-white min-h-screen w-full flex-col flex justify-center items-center space-y-5 sm:bg-blue-300">
-    <div class="w-full bg-white flex-col flex justify-center items-center p-5 sm:w-96 sm:rounded-xl">
+    <div class="w-full bg-white flex-col flex justify-center items-center p-5 lg:w-72 2xl:w-96 sm:rounded-xl">
         <form method="POST" class="space-y-5" action="{{ route('login') }}">
             @csrf
             <img class="min-w-full mb-5" src="{{ asset('img/logo.png') }}" alt="">
-            <input type="email" id="email" name="email" placeholder="Correo Electronico" class="border border-gray w-full rounded-md py-3 px-4" required>
-            <input type="password" id="password" name="password" placeholder="Contraseña" class="border border-gray w-full rounded-md py-3 px-4" required>
-            <input type="checkbox" id="remember" name='remember'><label for="remember"> Recuérdame</label>
+            <input type="email" id="email" name="email" placeholder="Correo Electronico" class="border border-gray w-full rounded-md py-3 px-4 text-sm 2xl:text-lg" required>
+            <input type="password" id="password" name="password" placeholder="Contraseña" class="border border-gray w-full rounded-md py-3 px-4 text-sm 2xl:text-lg" required>
+            <input type="checkbox" id="remember" name='remember'><label for="remember" class="text-sm 2xl:text-lg"> Recuérdame</label>
             <button class="min-w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600">
                 Iniciar sesión
             </button>
         </form>
     </div>
-    <div class="w-full bg-white flex justify-center items-center p-5 space-x-2 sm:w-96 sm:rounded-xl">
-        <p class="text-lg font-medium text-gray-700">¿No tienes una cuenta?</p>
-        <a class="text-lg font-medium text-blue-500" href="{{ route('signup') }}">Regístrate</a>
+    <div class="w-full bg-white flex justify-center items-center p-5 space-x-2 lg:w-72 2xl:w-96 sm:rounded-xl">
+        <p class="text-gray-700 text-sm 2xl:text-lg">¿No tienes una cuenta?</p>
+        <a class="text-blue-500 text-sm 2xl:text-lg" href="{{ route('signup') }}">Regístrate</a>
     </div>
 </div>
 </body>
