8c146d180e35993a070c78affcac2522a7127d4e
Thorristerki <thorristerki@gmail.com>
2021-07-29 02:27:43 -0400

    update views profile and provider create

--- resources/views/pages/providers/create.blade.php
+++ resources/views/pages/providers/create.blade.php
@@ -24,10 +24,10 @@
                 <p class="font-bold text-xl"><i class="fas fa-user-edit p-3 bg-white rounded-full border"></i> CREACION DE PROVEEDOR</p>
             </div>
             <div class="bg-white flex flex-col md:flex-row">
-                <div class="w-96 p-3 py-5 hidden md:block">
-                        <div class="flex flex-col space-y-3 items-center">
-                            <img class="w-60 border rounded-md" src="/img/provider.png">
-                        </div>
+                <div class="w-full md:w-96 p-3 py-5 hidden md:block">
+                    <div class="flex flex-col gap-3 items-center">
+                        <img class="w-60 h-60 border rounded-xl" src="/img/provider.png">
+                    </div>
                 </div>
                 <div class="w-full">
                     <form action="" method="POST">
@@ -35,25 +35,25 @@
                         <div class="flex-col p-3">
                             <p>Datos personales</p>
                         </div>
-                        <div class="flex flex-col space-y-3 md:flex-row md:space-y-0 md:space-x-5 p-3">
-                            <input type="text" id="nombre" name="nombre" placeholder="Nombre proveedor" class="border border-gray w-full rounded-md py-3 px-4">
-                            <input type="text" id="rut" name="rut" placeholder="Rut proveedor" class="border border-gray w-full rounded-md py-3 px-4">
+                        <div class="flex flex-col gap-3 p-3 lg:flex-row">
+                            <input type="text" id="nombre" name="nombre" placeholder="Nombre proveedor" class="border border-gray w-full rounded-xl py-3 px-4">
+                            <input type="text" id="rut" name="rut" placeholder="Rut proveedor" class="border border-gray w-full rounded-xl py-3 px-4">
                         </div>
-                        <div class="flex-col px-3 pt-3">
+                        <div class="flex-col p-3">
                             <p>Datos de Contactos</p>
                         </div>
-                        <div class="flex flex-col space-y-3 md:flex-row md:space-y-0 md:space-x-5 p-3">
-                            <input type="text" id="email" name="email" placeholder="E-Mail proveedor" class="border border-gray w-full rounded-md py-3 px-4">
-                            <input type="text" id="telefono" name="telefono" placeholder="Telefono proveedor" class="border border-gray w-full rounded-md py-3 px-4">
+                        <div class="flex flex-col gap-3 p-3 lg:flex-row">
+                            <input type="text" id="email" name="email" placeholder="E-Mail proveedor" class="border border-gray w-full rounded-xl py-3 px-4">
+                            <input type="text" id="telefono" name="telefono" placeholder="Telefono proveedor" class="border border-gray w-full rounded-xl py-3 px-4">
                         </div>
-                        <div class="flex flex-col space-y-3 md:flex-row md:space-y-0 md:space-x-5 p-3">
-                            <input type="text" id="direccion" name="direccion" placeholder="Direccion proveedor" class="border border-gray w-full rounded-md py-3 px-4">
-                            <input type="text" id="empresa_rrss" name="empresa_rrss" placeholder="RRSS proveedor" class="border border-gray w-full rounded-md py-3 px-4">
+                        <div class="flex flex-col gap-3 p-3 lg:flex-row">
+                            <input type="text" id="direccion" name="direccion" placeholder="Direccion proveedor" class="border border-gray w-full rounded-xl py-3 px-4">
+                            <input type="text" id="empresa_rrss" name="empresa_rrss" placeholder="RRSS proveedor" class="border border-gray w-full rounded-xl py-3 px-4">
                         </div>
-                        <div class="flex flex-col space-y-3 p-3">
-                            <input type="text" id="descripcion" name="descripcion" placeholder="Descripcion proveedor" class="border border-gray w-full rounded-md py-3 px-4">
+                        <div class="flex flex-col p-3">
+                            <input type="text" id="descripcion" name="descripcion" placeholder="Descripcion proveedor" class="border border-gray w-full rounded-xl py-3 px-4">
                         </div>
-                        <div class="flex flex-col space-y-3 p-3 items-center">
+                        <div class="flex flex-col p-3 items-center">
                             <button type="submit" class="w-full bg-blue-400 text-white py-3 px-6 rounded-full  md:w-52 hover:bg-blue-500">
                                 Actualizar datos
                             </button>
@@ -61,7 +61,7 @@
                     </form>
                 </div>
             </div>
-            <div class="bg-gray-50 shadow-lg p-2 md:rounded-b-3xl">
+            <div class="bg-gray-50 shadow-lg p-2 border-t md:rounded-b-3xl">
                 <p class="text-center text-gray-400">Komorebi</p>
             </div>
         </div>
