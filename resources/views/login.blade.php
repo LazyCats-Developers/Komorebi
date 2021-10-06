5dba3f047106b83db1e5accde1f10f3a550a2638
thorristerki <thorristerki@gmail.com>
2021-10-06 02:38:00 -0300

    win11 reload

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,7 +18,7 @@
             <input type="password" id="password" name="password" placeholder="Contraseña" class="border border-gray w-full rounded-md py-3 px-4 text-md 2xl:text-lg" required>
             <input type="checkbox" id="remember" name='remember'><label for="remember" class="text-sm 2xl:text-lg"> Recuérdame</label>
             <button class="min-w-full bg-blue-400 text-white font-semibold py-3 px-6 rounded-md hover:bg-green-600">
-                pico para el que lee
+                Iniciar sesión
             </button>
         </form>
     </div>
